0f5086980e8cf98e9d3e17c8fe3ca0e41663c761
Krzysztof Piasecki <krzysztof.piasecki1410@gmail.com>
2020-11-15 20:46:27 +0100

    Solved How Many Solutions

--- HackerRank/SimpleTasks/HowManySolutions.py
+++ HackerRank/SimpleTasks/HowManySolutions.py
@@ -1,29 +1,38 @@
+from collections import defaultdict
+
+first_solution = {}
+second_solution = {}
 table = [int(i) for i in input().split(" ")]
 n, x, y = table[0], table[1], table[2]
-
-solution = []
-i = 0
 
 a = b = c = d = 0
 
 count = 0
 overall = 0
-while n >= a:
-    while n >= b:
-        while n >= c:
-            while n >= d:
-                overall += 1
-                solution.append(str(a) + " " + str(b) + " " + str(c) + " " + str(d))
-                if x*a*a + y*b*b == x*c*c + y*d*d:
-                    count += 1
-                d += 1
-            c += 1
-            d = 0
-        b += 1
-        c = 0
+
+if n > 0 and x > 0 and y > 0:
+    while n >= a:
+        while n >= b:
+            equation = (x*a*a + y*b*b)
+            if equation in first_solution:
+                first_solution[equation] += 1
+            else:
+                first_solution[equation] = 1
+            b += 1
+        a += 1
+        b = 0
+    while n >= c:
+        while n >= d:
+            second_equation = (x*c*c + y*d*d)
+            if second_equation in second_solution:
+                second_solution[second_equation] += 1
+            else:
+                second_solution[second_equation] = 1
+            d += 1
+        c += 1
         d = 0
-    a += 1
-    b = 0
-    c = 0
-    d = 0
+    for x in second_solution:
+        if x in first_solution:
+            count += (first_solution[x] * second_solution[x])
+
 print(count)

44067868fca7cddfde70bede1f86e5824c73bc22
Krzysztof Piasecki <krzysztof.piasecki1410@gmail.com>
2020-11-16 11:04:42 +0100

    Solved JohnOrBetty

---
+++ HackerRank/SimpleTasks/JohnOrBetty.py
@@ -0,0 +1,10 @@
+table = [int(i) for i in input().split(" ")]
+john = int(table[0])
+betty = int(table[1])
+
+if john > betty:
+    print("John")
+elif betty > john:
+    print("Betty")
+else:
+    print("Nobody")

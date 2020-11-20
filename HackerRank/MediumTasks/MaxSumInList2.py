d69b5b48e92f6590a4d9a9a88f35c8566df7632e
Krzysztof <krzysztof.piasecki1410@gmail.com>
2020-11-20 17:36:02 +0100

    Solved MaxSumInList2 MaxSumInMatrix2

---
+++ HackerRank/MediumTasks/MaxSumInList2.py
@@ -0,0 +1,11 @@
+array = [int(i) for i in input().split(" ")]
+
+current_sum = 0
+largest_sum = 0
+for i in array:
+    current_sum += i
+    if current_sum < 0:
+        current_sum = 0
+    if current_sum > largest_sum:
+        largest_sum = current_sum
+print(largest_sum)
